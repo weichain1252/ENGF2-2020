f4dc14c8fd85d039385fbd451db34154b8462ef3
Mark Handley <mark@handley.org.uk>
2020-11-13 12:35:35 +0000

    Add transcript for C1

--- video3-python/src/gcd.py
+++ video3-python/src/gcd.py
@@ -8,30 +8,3 @@ def gcd(a, b):
         else:
             b = b - a
     return a
-
-def test_gcd():
-    ax = 42
-    bx = 30
-    v = gcd(ax, bx)
-    assert(v == 6)
-
-def test_gcd2():
-    ax = 6
-    bx = 6
-    v = gcd(ax, bx)
-    assert(v == 6)
-    
-def test_gcd3():
-    ax = 42
-    bx = -30
-    try:
-        v = gcd(ax, bx)
-        assert(False)
-    except ArithmeticError as e:
-        assert("Must be positive int." in str(e))
-    except:
-        assert(False)
-    finally:
-        assert(True)
-
-    
